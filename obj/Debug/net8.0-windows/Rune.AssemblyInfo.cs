13ba38f0fd9f3b0f7147d4ee6b2cc4566e8bed95
JustSomeGuest <JustSomeGuest@proton.me>
2026-09-22 15:04:21 +0300

    Add Rune project files

--- obj/Debug/net8.0-windows/Rune.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Rune.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rune")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39ae89a9306434a92565466ab6c0614831327918")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b6f409670e7baf084b89b5d67d10b3ea835e29d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rune")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rune")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
